68bb2eef37dc091e75809e3f7d61071563e4e44a
gstarkey20 <gstarkey1@catamount.wcu.edu>
2021-01-27 12:32:25 -0500

    it now prints a board to the console

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,8 +7,15 @@ Allows a user to play a complete game of TicTacToe.
 """
 
 class TicTacToe:
+	
+	# creating a board
+	board = [["_" for _ in range(3)] for _ in range(3)]
 
-	#TODO print the board
+	# prints a board to the console, utilizing list comprehension
+	def print_board(board):
+		[print(row) for row in board]	
+	print_board(board)
+		
 
 	#TODO determine whose turn it is
 
