1f3a065bb34035af6be9bfdf745f2e319ee16498
gstarkey20 <gstarkey1@catamount.wcu.edu>
2021-01-27 12:23:33 -0500

    updated game file with TODO comments

---
+++ tic_tac_toe.py
@@ -0,0 +1,17 @@
+"""
+@author Garrett Starkey
+@version 1
+
+Allows a user to play a complete game of TicTacToe. 
+
+"""
+
+class TicTacToe:
+
+	#TODO print the board
+
+	#TODO determine whose turn it is
+
+	#TODO determine a winner
+
+	#TODO print a winning message	
